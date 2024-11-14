69d9a0acc97315834a8c8875dd42ce3ea3a57ace
Ethosa <social.ethosa@gmail.com>
2024-11-14 23:48:17 +0700

    switch update checking to github api

--- mounts/updates.py
+++ mounts/updates.py
@@ -9,21 +9,22 @@ updates_app = FastAPI()
 
 
 @updates_app.get('/check')
-async def check_for_updates(md: bool = False):
+async def check_for_updates():
     session = ClientSession()
 
-    async with session.get('https://github.com/horanchikk/ktc-reborn/releases') as resp:
-        page_data = BeautifulSoup(await resp.text())
-        last_version = page_data.find('section').find('h2').text.strip()
-        description = clean_styles(page_data.find('section').find(
-            'div', {'class': 'markdown-body'}
-        )).encode_contents().decode('utf-8')
-        tag_info = page_data.select('section>div>:last-child>div>div>div>div>div>:last-child')
+    async with session.get('https://api.github.com/repos/horanchikk/ktc-reborn/releases') as resp:
+        data = (await resp.json())[0]
+        print(data)
 
     await session.close()
 
     return {
-        'version': last_version,
-        'description': markdownify(description) if md else description,
-        'tag_info': tag_info[0].text.strip().lower() if tag_info else 'last release'
+        'version': data['name'],
+        'description': data['body'],
+        'tag_info': 'last release' if not data['prerelease'] else 'pre release',
+        'apkfile': (
+            data['assets'][0]['browser_download_url']
+            if data['assets'] and data['assets'][0]['name'].endswith('.apk')
+            else ''
+        )
     }
